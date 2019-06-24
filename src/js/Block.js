fef4eeba8414d9d5838c635ca5034af97a904347
Łukasz Basaj <lbasaj@gmail.com>
2019-06-24 13:17:52 +0200

    add renderBlock() method

--- src/js/Block.js
+++ src/js/Block.js
@@ -6,6 +6,7 @@ class Block {
         this.colorOffset = 0;
         this.block = document.querySelector(".block");
     }
+
     getRandomBackground() {
         const chars = "0123456789ABCDEF";
         let color = "#";
@@ -13,6 +14,18 @@ class Block {
             color += chars[Math.floor(Math.random() * 16)];
         }
         return color;
+    }
+
+    renderBlock() {
+        this.size = this.position / 10;
+        this.block.style.height = `${this.size}vw`;
+        this.block.style.width = `${this.size}vw`;
+        this.block.style.left = `${this.position}%`
+        let nextColorOffset = (Math.round(this.block.offsetLeft / 50) * 50);
+        if (this.colorOffset !== nextColorOffset) {
+            this.colorOffset = nextColorOffset;
+            this.block.style.background = this.getRandomBackground();
+        }
     }
 }
 
