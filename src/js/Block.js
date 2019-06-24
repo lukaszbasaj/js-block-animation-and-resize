098a2b8851144a517c3212b4b3ff8a6514a9be7f
Łukasz Basaj <lbasaj@gmail.com>
2019-06-24 13:07:10 +0200

    fill Block class

--- src/js/Block.js
+++ src/js/Block.js
@@ -1,6 +1,10 @@
 class Block {
     constructor() {
-
+        this.size = 0;
+        this.position = 0;
+        this.moveToLeft = true;
+        this.colorOffset = 0;
+        this.block = document.querySelector(".block");
     }
 }
 
