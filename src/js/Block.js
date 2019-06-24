cca2f2760dd0e48296c62be4603dbbcec3efb6d2
Łukasz Basaj <lbasaj@gmail.com>
2019-06-24 13:12:28 +0200

    add getRandomBackgroud() method

--- src/js/Block.js
+++ src/js/Block.js
@@ -6,6 +6,14 @@ class Block {
         this.colorOffset = 0;
         this.block = document.querySelector(".block");
     }
+    getRandomBackground() {
+        const chars = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+            color += chars[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
 }
 
 export default Block;
